ba6ee9f948811203ae744343b22a605c1d655a8f
AR Gorsi <argorsi707@gmail.com>
2024-05-12 14:39:14 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="main-body">
+        <div className="circle"></div>
+        <div className=" last-circle"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
